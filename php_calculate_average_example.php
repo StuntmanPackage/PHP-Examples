6c2c3ea77314b724bfc68ec837df92a13fa701e7
StuntmanPackage <info@roblammerts.nl>
2021-02-16 20:24:54 +0100

    Added average lap time calculator example
    Example to calculate the average lap time with PHP.

---
+++ php_calculate_average_example.php
@@ -0,0 +1,13 @@
+<?php
+/* Have fun, made by StuntmanPackage 
+Date: February 16 2021  */
+
+	$round1 = 42;
+	$round2 = 38;
+	$round3 = 36;
+	$total = $round1 + $round2 + $round3;
+	$average = round(($total / 3), 1);
+	
+	echo "Total time over the three laps is: " . $total . " seconds. <br />";
+	echo "The average lap time is: " .$average . " seconds.";
+?>
