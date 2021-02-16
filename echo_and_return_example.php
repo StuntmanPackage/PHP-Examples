c47a886193e5e6ebbd03badb40922ec3809825a8
StuntmanPackage <info@roblammerts.nl>
2021-02-16 21:03:54 +0100

    Added Echo and Return example
    Showing the difference between echo and return in PHP.

---
+++ echo_and_return_example.php
@@ -0,0 +1,22 @@
+<?php
+/* Have fun, made by StuntmanPackage 
+Date: February 16 2021  */
+
+/* Echo */
+echo 'Cat ';
+echo 'Dog ';
+
+$name = 'John ';
+echo $name;
+echo 3 + 2;
+
+echo "<br/>";
+
+/* Return */
+function test() {
+    return ' Johnny';
+  }
+
+  $name = test();
+  echo $name;
+?>
