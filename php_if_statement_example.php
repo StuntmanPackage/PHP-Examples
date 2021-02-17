1af19ffdc293caf908b7c82705a1620c2b8cb41c
StuntmanPackage <info@roblammerts.nl>
2021-02-17 21:16:35 +0100

    Added if statement example
    Perfect example of using if statements within PHP.

---
+++ php_if_statement_example.php
@@ -0,0 +1,13 @@
+<?php
+/* Have fun, made by StuntmanPackage 
+Date: February 17 2021  */
+
+	$age = 18;
+	$driverLicense = true; /* Change this to true and see what happens! */
+
+	if ($age >= 18 && $driverLicense){
+		echo "You may drive a car.";
+	} else{
+		echo "You are now allowed to drive a car!";
+	}
+?>
