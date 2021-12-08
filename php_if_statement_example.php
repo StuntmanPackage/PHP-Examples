26538e5f03127a631648a5ece81648b83a4f0ec4
StuntmanPackage <info@roblammerts.nl>
2021-12-08 13:33:06 +0100

    Small update

--- php_if_statement_example.php
+++ php_if_statement_example.php
@@ -4,7 +4,7 @@
  * Date: February 17 2021 
  */
 
-	$age = 18;
+	$age = 18; /* Change this below 18 and see what happens! */
 	$driversLicense = false; /* Change this to true and see what happens! */
 
 	if ($age >= 18 && $driversLicense){
